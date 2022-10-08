e48d26be8a1052a8d4139556fda58201b6abbaf6
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-10-08 21:45:31 +0300

    test ci48

--- tests.py
+++ tests.py
@@ -4,4 +4,4 @@ def test_expected_page():
     with open('output.html', "r", encoding='utf-8-sig') as file:
         output_page = file.readlines()[:-1]
 
-    assert expected_page == output_page
+    assert expected_page == '\n'.join(output_page)
