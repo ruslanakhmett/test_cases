6cf9c2861c868a7a8492f5c68ea122ff82f9d781
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-10-08 20:28:28 +0300

    test ci30

---
+++ tests.py
@@ -0,0 +1,7 @@
+def test_download():
+    with open('fixture.html', "r", encoding="utf-8") as file:
+                expected_page = file.read()
+    with open('output.html', "r", encoding="utf-8") as file:
+                output_page = file.read()
+
+    assert expected_page == output_page
