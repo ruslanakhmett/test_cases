d06d02a6221f55227913ba48e894c0d3bf2a1609
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-10-08 21:29:25 +0300

    test ci44

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 def test_expected_page():
-    with open('fixture.html', "r", encoding='utf-8') as file:
+    with open('tt.html', "r", encoding='utf-8-sig') as file:
         expected_page = file.read()
     with open('output.html', "r", encoding='utf-8-sig') as file:
         output_page = file.read()
